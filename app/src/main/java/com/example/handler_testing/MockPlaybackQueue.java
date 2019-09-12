22706d057b3278998721c9d2e2dac355739975e0
Edward Lu <edwardzlu98@gmail.com>
2019-09-12 11:52:25 -0400

    Figured out issue, scheduleNextWork was passed with wrong parameter, fixed

--- app/src/main/java/com/example/handler_testing/MockPlaybackQueue.java
+++ app/src/main/java/com/example/handler_testing/MockPlaybackQueue.java
@@ -3,6 +3,7 @@ package com.example.handler_testing;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
+import android.os.SystemClock;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
@@ -11,12 +12,26 @@ public class MockPlaybackQueue {
 
     private static final String TAG = "MockPlaybackQueue";
 
-    // Messages
-    public static final int MSG_TEST = 0;
+    // Private constants
+    private static final int PROCESSING_INTERVAL_MS = 1000;
 
+    // Messages
+    public static final int MSG_DO_SOME_WORK = 0;
+    public static final int MSG_TEST = 1;
+
+    private Handler progressEventsHandler_;
     private Handler moduleMessageHandler_;
+    private Handler workHandler_;
 
     public MockPlaybackQueue(Looper workThreadLooper) {
+
+        progressEventsHandler_ = new Handler(workThreadLooper) {
+            @Override
+            public void handleMessage(@NonNull Message msg) {
+                super.handleMessage(msg);
+            }
+        };
+
         moduleMessageHandler_ = new Handler(workThreadLooper) {
             @Override
             public void handleMessage(@NonNull Message msg) {
@@ -31,10 +46,38 @@ public class MockPlaybackQueue {
                 }
             }
         };
+
+        workHandler_ = new Handler(workThreadLooper) {
+            @Override
+            public void handleMessage(@NonNull Message msg) {
+                switch (msg.what) {
+                    case MSG_DO_SOME_WORK: {
+                        doSomeWork();
+                        break;
+                    }
+                    default: {
+                        throw new IllegalStateException("unexpected msg.what " + msg.what);
+                    }
+                }
+            }
+        };
+
+        doSomeWork(); // start the workHandler's work cycle
+
     }
 
     public void sendTestMessage() {
         moduleMessageHandler_.obtainMessage(MSG_TEST).sendToTarget();
+    }
+
+    public void doSomeWork() {
+        Log.d(TAG, "doSomeWork called");
+        scheduleNextWork(SystemClock.uptimeMillis() + PROCESSING_INTERVAL_MS);
+    }
+
+    private void scheduleNextWork(long thisOperationStartTimeMs) {
+        workHandler_.removeMessages(MSG_DO_SOME_WORK);
+        workHandler_.sendEmptyMessageAtTime(MSG_DO_SOME_WORK, thisOperationStartTimeMs + PROCESSING_INTERVAL_MS);
     }
 
 }
